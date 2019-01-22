8789dd3115d217e3ba01528ec77450e7076f51e9
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-01-22 22:42:12 +0530

    vowels.c

---
+++ vowels.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+ void main()
+ {
+ char a;
+ scanf("%c",&a);
+ if(a=='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U')
+ {
+ printf("vowels");
+ }
+ else
+ {
+ printf("consonent");
+ }
+ getch();
+ }
