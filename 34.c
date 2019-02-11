36e1ecb039af563a2f6535b56781b9cf667dd7b3
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-11 17:33:15 +0530

    34.c

---
+++ 34.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+   char s[100];
+   int l,c=1,i;
+   scanf("%s,s");
+   l=strlen(s);
+   for(i=0;i<l;i++)
+   {
+       if(s[i]=='.')
+       {
+       c++;
+       printf("%d",c);
+       }
+       
+       else
+       {
+       printf("%d",c);
+       }
+   }
+
+}
