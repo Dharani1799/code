2c4ff72dccb0825974862c581726bf6cc6e876dc
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-11 18:27:05 +0530

    39.c

---
+++ 39.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+  void main()
+  {
+    int a[10];
+    int i;
+    int g;
+    printf("Enter ten values:");
+
+    for (i = 0; i < 10; i++) {
+    scanf("%d", &a[i]);
+    }
+    
+    g = a[0];
+    for (i = 0; i < 10; i++) {
+if (a[i] > g) {
+g = a[i];
+    }
+    }
+    printf(" %d", g);
+
+  }
